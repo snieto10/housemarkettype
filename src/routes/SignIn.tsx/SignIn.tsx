944786ad38a6b6e5ec359bc6438c3acb8829dc2b
Sergio Nieto <snieto10@gmail.com>
2023-04-13 18:42:02 -0400

    mnember

--- src/routes/SignIn.tsx/SignIn.tsx
+++ src/routes/SignIn.tsx/SignIn.tsx
@@ -1,22 +1,37 @@
 import { useState } from 'react';
+import './signIn.css';
 
 function SignIn() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   return (
     <>
-      <div>
-        <h1>Member Sign In</h1>
-        <p>Dont have an account? Create free account</p>
-        <input type='text' />
-        <br />
-        <input type='password' />
-        <br />
-        <button>Sign In</button>
+      <div className=' SIbackground'>
+        <div className='SIbox'>
+          <div className='SIx'>X</div>
+          <h1 className='SItitle'>Member Sign In</h1>
+          <p className='SIpara'>Dont have an account? Create free account</p>
+          <div className='SIwidth'>
+            <form>
+              <label htmlFor='email'>Email:</label>
+              <input className='SIinput1' name='email' id='email' type='text' />
+              <br />
+              <label htmlFor='password'>Password:</label>
+              <input
+                name='password'
+                id='password'
+                className='SIinput2'
+                type='password'
+              />
+            </form>
+          </div>
+          <br />
+          <button>Sign In</button>
 
-        <p>or</p>
+          <p>or</p>
 
-        <button>Google</button>
+          <button>Google</button>
+        </div>
       </div>
     </>
   );
